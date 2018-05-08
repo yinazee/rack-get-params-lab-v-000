62e216caf1bc1ed34c2c35ba95b3fa7ad62f4789
Yina Zhu <yinagit@gmail.com>
2018-05-08 19:32:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -21,6 +21,7 @@ class Application
       if @@cart.empty?
         resp.write "Your cart is empty"
       else
+        binding.pry
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
